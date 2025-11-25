3bf2ed9c440302d6eb64cdd78975c9b82a0a3958
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-11-25 10:15:42 -0300

    Controller inserirUsuario OK

--- controller/usuario/controller_usuario.js
+++ controller/usuario/controller_usuario.js
@@ -81,7 +81,64 @@ const buscarUsuarioId = async (id) => {
 
 // buscarUsuarioId(3);
 
+const novoUsuario = {
+  nome: "Rebeca",
+  genero: "Feminino",
+  email: "rebeca@email.com",
+  senha: "senha456",
+  administrador: 1,
+  contentType: "application/json",
+};
+
+//insere um usuário
+/* Vazio, undefined e null estão sendo cadastrados no banco. Tratar pelo front */
+const inserirUsuario = async (usuario, contentType) => {
+  //criando um objeto novo para as mensagens
+  let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES));
+
+  try {
+    //validação do tipo de conteudo da requisição (obrigatorio ser um json)
+    if (String(contentType).toUpperCase() == "APPLICATION/JSON") {
+      //chama a função para inserir um novo usuario no DB
+      let resultUsuarios = await usuarioDAO.setInsertUser(usuario);
+
+      if (resultUsuarios) {
+        //chama a função para receber o ID gerado no DB
+        let lastID = await usuarioDAO.getSelectLastId();
+
+        if (lastID) {
+          //adiciona o ID no JSON com os dados do usuário
+          usuario.id = lastID;
+          MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATED_ITEM.status;
+          MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATED_ITEM.status_code;
+          MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_CREATED_ITEM.message;
+
+          MESSAGES.DEFAULT_HEADER.items = usuario;
+          console.log(MESSAGES.DEFAULT_HEADER);
+
+          return MESSAGES.DEFAULT_HEADER; //201
+        } else {
+          console.log(MESSAGES.ERROR_INTERNAL_SERVER_MODEL);
+          return MESSAGES.ERROR_INTERNAL_SERVER_MODEL; //500
+        }
+      } else {
+        console.log(MESSAGES.ERROR_INTERNAL_SERVER_MODEL);
+        return MESSAGES.ERROR_INTERNAL_SERVER_MODEL; //500
+      }
+    } else {
+      console.log(MESSAGES.ERROR_CONTENT_TYPE);
+      return MESSAGES.ERROR_CONTENT_TYPE; //415
+    }
+  } catch (error) {
+    console.log(MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER);
+    return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER; //500
+  }
+};
+
+inserirUsuario(novoUsuario, novoUsuario.contentType);
+
 module.exports = {
   listarUsuarios,
   buscarUsuarioId,
+  inserirUsuario,
 };
